3f6a1722c64e44117261357a18911a221cc2930d
MateoB91 <mateobianco91100@gmail.com>
2022-06-10 16:52:14 +0200

    new commit

--- public/js/index.js
+++
@@ -1,8 +0,0 @@
-let burger = document.getElementById('clickjs');
-let absolute = document.querySelector('.abso')
-
-
-burger.addEventListener("click", () => {
-    absolute.classList.toggle('hidden');
-});
-
